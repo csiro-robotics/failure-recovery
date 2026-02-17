149513ce0c2545ea11e48da76f23b9893d438f1d
Fahira Afzal Maken <fahira.afzalmaken@data61.csiro.au>
2026-02-17 17:38:57 +1100

    Speakers' info update

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -103,13 +103,13 @@ const Contact: React.FC = () => {
 
 const BASE = import.meta.env.BASE_URL;
 const keynoteSpeakers = [
-  { name: "David Hsu", affiliation: "National University of Singapore", image: `${BASE}speakers/David_Hsu.jpeg` },
-  { name: "Fabio Ramos", affiliation: "University of Sydney, NVIDIA-USA", image: `${BASE}speakers/Fabio_Ramos.jpeg` },
-  { name: "Abhinav Valada", affiliation: "University of Freiburg", image: `${BASE}speakers/Abhinav_Valada.jpeg` },
-  { name: "Stefan Leutenegger", affiliation: "ETH Zurich", image: `${BASE}speakers/Stefan_Leutenegger.jpeg` },
-  { name: "Marco Pavone", affiliation: "Stanford University, NVIDIA-USA", image: `${BASE}speakers/Marco_Pavone.jpeg` },
+  { name: "David Hsu", affiliation: "National University of Singapore", image: `${BASE}speakers/David_Hsu.jpeg`,url: "https://www.comp.nus.edu.sg/~dyhsu/" },
+  { name: "Fabio Ramos", affiliation: "University of Sydney, NVIDIA-USA", image: `${BASE}speakers/Fabio_Ramos.jpeg`,url:"https://fabioramos.github.io/Home.html" },
+  { name: "Abhinav Valada", affiliation: "University of Freiburg", image: `${BASE}speakers/Abhinav_Valada.jpeg`,url:"https://rl.uni-freiburg.de/people/valada" },
+  { name: "Stefan Leutenegger", affiliation: "ETH Zurich", image: `${BASE}speakers/Stefan_Leutenegger.jpeg`,url:"https://scholar.google.com/citations?user=SmGQ48gAAAAJ&hl=en" },
+  { name: "Marco Pavone", affiliation: "Stanford University, NVIDIA-USA", image: `${BASE}speakers/Marco_Pavone.jpeg`,url:"https://web.stanford.edu/~pavone/index.html" },
   { name: "Will Maddern", affiliation: "Wayve", image: `${BASE}speakers/Will_Maddern.jpeg` },
-  { name: "Micol Spitale", affiliation: "Polytecnico di Milano", image: `${BASE}speakers/Micol_Spitale.jpeg` },
+  { name: "Micol Spitale", affiliation: "Polytecnico di Milano", image: `${BASE}speakers/Micol_Spitale.jpeg`,url:"https://spitale.faculty.polimi.it/" },
 ];
 
 
@@ -128,10 +128,13 @@ const KeynoteSpeakers: React.FC = () => {
           alt={p.name}
           className={ks.photo}
         />
-        <p className={ks.name}>{p.name}</p>
-        <p className={ks.affiliation}>{p.affiliation}</p>  {/* new line */}
-   
-      </div>
+        <p className={ks.name}>
+        <a href={p.url} target="_blank" rel="noopener noreferrer">
+          {p.name}
+        </a>
+      </p>
+      <p className={ks.affiliation}>{p.affiliation}</p>
+    </div>
     ))}
   </div>
     </section>
